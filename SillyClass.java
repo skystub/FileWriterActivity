5955f97d35171b668c1552fc8d5ae5a9aa966dd9
sky <sstubbeman1@hwemail.com>
2024-09-06 14:43:54 -0700

    Fixed silly message
    no longer silly

--- SillyClass.java
+++ SillyClass.java
@@ -1,6 +1,6 @@
 public class SillyClass {
     public static void main(String[] args) {
         System.out.println("This class is a silly class.");
-        System.out.println("this sure is a silly class");
+        System.out.println("this sure is NOT a silly class");
     }
 }
